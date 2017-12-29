8a1498fb0faa866591972319d05cebf9311d7457
Sean West <swest06@dcs.bbk.ac.uk>
2017-12-29 02:58:37 +0000

    write convertToInts() pytest

--- test_Sudoku2.py
+++ test_Sudoku2.py
@@ -7,3 +7,8 @@ def test_convertToSets():
     new_array = [[zero, {1}, {2}], [{1}, zero, {2}], [zero, {1}, zero]]
     assert new_array == convertToSets(array)
 
+
+def test_convertToInts():
+    sets = [[{1, 2}, {3}, {4}], [{1}, {3, 5, 7}, {2}], [{2, 3}, {2}, {3}]]
+    new_set = [[0 , 3, 4], [1, 0, 2], [0, 2, 3]]
+    assert new_set == convertToInts(sets)

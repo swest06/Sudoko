dc8b4ee4e0517bf2551aed24c90594a3d4c270f4
Sean West <swest06@dcs.bbk.ac.uk>
2018-01-26 02:07:04 +0000

    add getRowLocations() and getColumnLocations()

--- sudoku.py
+++ sudoku.py
@@ -12,15 +12,29 @@ def convertToSets(problem):
 
 
 def convertToInts(problem):
-    
     for lst in problem:
         for i, x in enumerate(lst):
             if len(x) > 1:
                 lst[i] = 0
             else:
-                x = str(x)
-                x = x.replace('{', '').replace('}', '')
-                x = int(x)
-                lst[i] = x
+                x = str(x.replace('{', '').replace('}', ''))
+                lst[i] = int(x)
     
     return problem
+
+
+def getRowLocations(rowNumber):
+    r = rowNumber
+    lst =  [(r, 0), (r, 1), (r, 2), (r, 3), (r, 4), (r, 5), (r, 6), (r, 7), (r, 8)]
+    
+    return lst
+
+
+def getColumnLocations(columnNumber):
+    c = columnNumber
+    lst = [(0, c), (1, c), (2, c), (3, c), (4, c), (5, c), (6, c), (7, c), (8, c)]
+
+    return lst
+
+
+def def getBoxLocations(location):
